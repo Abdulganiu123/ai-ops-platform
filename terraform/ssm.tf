df11a26c22a48d03ece9ae53c6efc8eeab98326e
Abdulganiu Agboola <agboolaganiu247@gmail.com>
2026-08-15 17:35:23 -0600

    Add checkov suppressions, pin scanner version

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,4 +1,5 @@
 resource "aws_ssm_parameter" "guardrail" {
+  # checkov:skip=CKV2_AWS_34: guardrail id is a non-secret identifier, not a credential
   name        = "/${var.project_name}/guardrail"
   description = "Guardrail id and version for the devgen CLI to apply."
   type        = "String"
